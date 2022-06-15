62f26e16bf42f9af87f33fc51ace8efeac70672a
Mao <mauricio.vera.r7@gmail.com>
2022-06-15 13:07:01 -0400

    lupa

--- js/poke.js
+++ js/poke.js
@@ -20,16 +20,57 @@ const datos = (i) => {
     const ancho = datos.weight
     const arr = [i, nom, tipo, alto, ancho]
     div_cuadro.innerHTML = `
-  <h1>${nom}</h1>
-  <img id=${i} onclick="datos('${i}')" src="http://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/${i}.png">
-  <h5>Types</h5>
-  <ul>
-    <li>${tipo}</li>
-  </ul>
-  <h5>Height</h5>
-  <p>${alto}</p>
-  <h5>weight</h5>
-  <p>${ancho}</p>
-  `
+      <h1>${nom}</h1>
+      <img id=${i} onclick="datos('${i}')" src="http://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/${i}.png">
+      <h5>Types</h5>
+      <ul>
+        <li>${tipo}</li>
+      </ul>
+      <h5>Height</h5>
+      <p>${alto}</p>
+      <h5>weight</h5>
+      <p>${ancho}</p>
+      <!-- Modal -->
+      <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+              <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+              ...
+            </div>
+            <div class="modal-footer">
+              <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+              <button type="button" class="btn btn-primary">Save changes</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    ` // hacerlo con createElement, setAtribute, getAtribute, querySelectorAll
+  })
+}
+
+const myModal = document.getElementById('myModal')
+const myInput = document.getElementById('myInput')
+
+// myModal.addEventListener('click', () => {
+// })
+
+// $("#myModal").on("shown.bs.modal", function () { 
+//   alert('Hi');
+// }).modal('show')
+
+let lupas = document.querySelectorAll(".pokes > img")
+for (let j = 0; j < lupas.length; j++) {
+  lupas[j].addEventListener("mouseover", function () {
+    this.classList.add('lupa')
+  })
+}
+
+for (let k = 0; k < lupas.length; k++) {
+  lupas[k].addEventListener("mouseout", function () {
+    this.classList.remove('lupa')
   })
 }
